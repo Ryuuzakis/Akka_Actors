03804afd0f48757cc04f10153bf34c524d3102fa
Louis Guilbert <louis.guilbert@free.fr>
2017-03-26 19:54:35 +0200

    sbt config files

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -16,14 +16,25 @@ object Main extends App {
   calculate()
 
   def calculate() = {
-    setupSys1()
-    setupSys2()
+    if(args(0).equals("sys1")) {
+      setupSys1()
+    } else if(args(0).equals("sys2")) {
+      setupSys2()
+    } else if(args(0).equals("console")) {
+      console()
+    }
+  }
+
+  def loadConf(conf: String) = {
+    val configFile = getClass.getClassLoader.getResource(conf).getFile
+    ConfigFactory.parseFile(new File(configFile))
+  }
+  
+  def console() = {
     val local = ActorSystem("LocalConsole", loadConf("local.conf"))
     val system1Path = "akka.tcp://RemoteSystem1@localhost:5150/user/"
     val system2Path = "akka.tcp://RemoteSystem2@localhost:5151/user/"
-    
-    //Thread.sleep(4000);
-    
+        
     local.actorSelection(system1Path + "node0") ! AddNeighbours(List(system1Path + "node1", system2Path + "node4"))
     local.actorSelection(system1Path + "node1") ! AddNeighbours(List(system1Path + "node0", system1Path + "node2", system2Path + "node3"))
     local.actorSelection(system1Path + "node2") ! AddNeighbours(List(system1Path + "node1"))
@@ -35,26 +46,19 @@ object Main extends App {
     local.actorSelection(system1Path + "node0") ! TextMessage(0)
   }
 
-  def loadConf(conf: String) = {
-    val configFile = getClass.getClassLoader.getResource(conf).getFile
-    ConfigFactory.parseFile(new File(configFile))
-  }
-
   def setupSys1() = {
     val system1 = ActorSystem("RemoteSystem1", loadConf("system1.conf"))
-    val system1Addr = AddressFromURIString("akka.tcp://RemoteSystem1@localhost:5150")
     for (i <- 0 to 2) {
       println("actor " + i + " on system1")
-      system1.actorOf(Props(new Node(i)).withDeploy(Deploy(scope = RemoteScope(system1Addr))), name = "node" + i)
+      system1.actorOf(Props(new Node(i)), name = "node" + i)
     }
   }
 
   def setupSys2() = {
     val system2 = ActorSystem("RemoteSystem2", loadConf("system2.conf"))
-    val system2Addr = AddressFromURIString("akka.tcp://RemoteSystem2@localhost:5151")
     for (i <- 3 to 5) {
       println("actor " + i + " on system2")
-      system2.actorOf(Props(new Node(i)).withDeploy(Deploy(scope = RemoteScope(system2Addr))), name = "node" + i)
+      system2.actorOf(Props(new Node(i)), name = "node" + i)
     }
   }
 }
